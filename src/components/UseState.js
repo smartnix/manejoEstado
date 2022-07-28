49d8c4edcdd700eb72920e7e30fa270ec2423f45
smartnix <smartnix@hotmail.com>
2022-07-28 08:55:52 -0500

    Estados imperativos con useState de varias pantallas

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -7,7 +7,9 @@ function UseState({name})
     const [state, setState] = useState({
         value:'',
         error:false,
-        loading:false
+        loading:false,
+        delete:false,
+        confirmed:false
     });
 
     useEffect(() => {
@@ -19,10 +21,10 @@ function UseState({name})
                 if(state.value !== SECURITY_CODE)
                 {
                     setState({...state,error:true, loading:false})
-                }
+                } 
                 else
                 {
-                    setState({...state,loading:false,error:false})
+                    setState({...state,loading:false,error:false, confirmed:true})
                 }
                 }, 2000);
         }
@@ -32,34 +34,79 @@ function UseState({name})
         setState({...state,value:event.target.value})
      };
     
-    return (
-        <div>
-            <h2>Eliminar UseState</h2>
-            <p>Por favor, escribe el codigo de seguridad</p>
-
-            {
-                (state.error && !state.loading) && 
-                (
-                    <p>Error: el codigo es incorrecto</p>
-                )
-            }
-
-            {   
-                state.loading  &&
-                (
-                    <p>Cargando...</p>
-                )
-            }
-
-            <input placeholder="Código de seguridad" 
-                value={state.value} 
-                onChange={onChangeHandler}
-            />
-            <button
-                onClick={() =>  setState({...state,loading:true}) }  
-            >Comprobar</button>
-        </div>
-    );
+    if(!state.deleted && !state.confirmed)
+    {
+        return (
+            <div>
+                <h2>Eliminar UseState</h2>
+                <p>Por favor, escribe el codigo de seguridad</p>
+    
+                {
+                    (state.error && !state.loading) && 
+                    (
+                        <p>Error: el codigo es incorrecto</p>
+                    )
+                }
+    
+                {   
+                    state.loading  &&
+                    (
+                        <p>Cargando...</p>
+                    )
+                }
+    
+                <input placeholder="Código de seguridad" 
+                    value={state.value} 
+                    onChange={onChangeHandler}
+                />
+                <button
+                    onClick={() =>  setState({...state,loading:true}) }  
+                >Comprobar</button>
+            </div>
+        );
+    }
+    else if(state.confirmed && !state.deleted)
+    {
+        return (
+            <React.Fragment>
+                <p>Confirmación, ¿Estas seguro?</p>
+                <button
+                    onClick={()=>{setState({
+                            ...state,
+                            deleted:true
+                        })
+                    }}
+                >Si, Elimnar</button>
+                <button
+                    onClick={()=>{setState({
+                        ...state,
+                        confirmed:false,
+                        value:''
+                    })
+                }}
+                >No, Elimnar</button>
+            </React.Fragment>
+        );
+    }
+    else
+    {
+        return (
+            <React.Fragment>
+                <p>Eliminado con exito</p>
+                <button
+                     onClick={()=>{setState({
+                        ...state,
+                        confirmed:false,
+                        deleted:false,
+                        value:''
+                    })
+                }}
+                >
+                    Resetear, volver
+                </button>
+            </React.Fragment>
+        );
+    }
 
 }
 
